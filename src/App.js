41d63e685083768279ef4443da68beea806e3820
jagdishlove <jagdish.funn@gmail.com>
2021-04-23 14:58:03 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import './App.css';
-import Row from './Components/Row/Row'
-import requests from './request';
-import Banner from './Components/Banner/Banner'
-import NavBar from './Components/NavBar/NavBar';
-import Footer from './Components/Footer/Footer'
+import "./App.css";
+import Row from "./Components/Row/Row";
+import requests from "./request";
+import Banner from "./Components/Banner/Banner";
+import NavBar from "./Components/NavBar/NavBar";
+import Footer from "./Components/Footer/Footer";
 function App() {
-
   return (
     <div className="App">
-     <NavBar/>
-      {/* Banner */}
-      <Banner/>
-      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}
-      isLargeRow/>
-      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
-      <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
-      <Row title="Action" fetchUrl={requests.fetchActionMovies}/>
-      <Row title="Comedy" fetchUrl={requests.fetchComedyMovies}/>
-      <Row title="Horror" fetchUrl={requests.fetchHorrorMovies}/>
-      <Row title="Romance" fetchUrl={requests.fetchRomanceMovies}/>
-      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}/>
-
-    <Footer/>
+      <NavBar />
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+      <Footer />
     </div>
   );
 }
